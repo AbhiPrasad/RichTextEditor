772bf792b1fddb3d69d7fb79bf09899c20bea4db
Abhijeet Prasad <abhijeetprasad98@gmail.com>
2018-01-21 23:14:38 -0500

    Add exec command for bold and italics

--- src/components/RichTextEditor/RichTextEditor.tsx
+++ src/components/RichTextEditor/RichTextEditor.tsx
@@ -2,21 +2,49 @@ import * as React from 'react';
 import { Editor, Toolbar } from './components';
 import './RichTextEditor.css';
 
-class RichTextEditor extends React.PureComponent {
+interface State {
+  html: string | null;
+}
+
+export interface Props {}
+
+class RichTextEditor extends React.PureComponent<Props, State> {
+  state: State = {
+    html: null,
+  };
+
+  private Editor: HTMLDivElement | null = null;
+
   render() {
+    // const {html} = this.state;
     return (
       <div className="Richtexteditor">
-        <Toolbar />
+        <Toolbar 
+          onBoldClick={this.handleBoldClick}
+          onItalicClick={this.handleItalicClick}
+        />
         <Editor 
           onChange={this.handleInputChange}
+          // html={html}
+          isBold={false}
         />
       </div>  
     );
   }
 
+  private handleItalicClick = () => {
+    document.execCommand('italic', false);
+  }
+
+  private handleBoldClick = () => {
+    document.execCommand('bold', false);
+  }
+
   private handleInputChange = (event: React.FormEvent<HTMLElement>) => {
+    // tslint:disable-next-line
+    console.log(event.currentTarget.innerText, this.Editor);
     this.setState({
-      html: event.currentTarget.innerHTML ? event.currentTarget.innerHTML : '',
+      html: event.currentTarget.innerText ? event.currentTarget.innerText : null,
     });
   }
 }
