e3d01043c564d0b5969cb9d64c77b5b9296215a9
Abhijeet Prasad <abhijeetprasad98@gmail.com>
2018-01-22 00:59:22 -0500

    Get simple links working

--- src/components/RichTextEditor/RichTextEditor.tsx
+++ src/components/RichTextEditor/RichTextEditor.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
+import * as ReactModal from 'react-modal';
 import { Editor, Toolbar } from './components';
 import './RichTextEditor.css';
 
 interface State {
   html: string | null;
+  showModal: boolean;
+  url: string;
+  anchorOffset: number;
+  focusOffset: number;
+  anchorNode: Node | null;
+  focusNode: Node | null;
 }
 
 export interface Props {}
@@ -11,25 +18,91 @@ export interface Props {}
 class RichTextEditor extends React.PureComponent<Props, State> {
   state: State = {
     html: null,
+    showModal: false,
+    url: '',
+    anchorOffset: 0,
+    focusOffset: 0,
+    anchorNode: null,
+    focusNode: null,
   };
 
   private Editor: HTMLDivElement | null = null;
 
   render() {
-    // const {html} = this.state;
+    const {showModal, url} = this.state;
     return (
       <div className="Richtexteditor">
         <Toolbar 
           onBoldClick={this.handleBoldClick}
           onItalicClick={this.handleItalicClick}
+          onLinkClick={this.handleLinkClick}
         />
         <Editor 
-          onChange={this.handleInputChange}
-          // html={html}
-          isBold={false}
+          setEditor={this.handleSetEditor}
+          onMouseChange={this.handleMouseChange}
         />
+        <ReactModal 
+          isOpen={showModal}
+          ariaHideApp={false}
+        >
+          <p>Insert url:</p>
+          <input type="text" onChange={this.handleUrlChange} value={url}/>
+
+          <button onClick={this.handleInsert} type="button"> Insert Link </button>
+          <button onClick={this.handleRemove} type="button"> Remove Link </button>
+        </ReactModal>
       </div>  
     );
+  }
+
+  private handleMouseChange = (event: React.FormEvent<HTMLElement>) => {
+    this.setState({
+      anchorOffset: window.getSelection().anchorOffset,
+      focusOffset: window.getSelection().focusOffset,
+      anchorNode: window.getSelection().anchorNode,
+      focusNode: window.getSelection().focusNode,
+    });
+  }
+
+  private handleSetEditor = (element: HTMLDivElement) => {
+    this.Editor = element;
+  }
+
+  private handleUrlChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({url: event.currentTarget.value});
+  }
+
+  private handleLinkClick = () => {
+    this.setState({showModal: true});
+  }
+
+  private handleInsert = () => {
+    const {anchorNode, focusNode, anchorOffset, focusOffset} = this.state;
+
+    // tslint:disable-next-line
+    console.log(this.Editor!.childElementCount);
+    // tslint:disable-next-line
+    console.log('anchor:', anchorOffset);
+    // tslint:disable-next-line
+    console.log('focus:', focusOffset);
+
+    // Anchor is where user begins selection and focus is where user ends selection
+
+    this.Editor!.focus();
+    var range = document.createRange();
+    range.setStart(anchorNode!, anchorOffset);
+    range.setEnd(focusNode!, focusOffset);
+    var sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+
+    document.execCommand('createLink', false, this.state.url);
+    this.setState({showModal: false, url: ''});
+  }
+
+  private handleRemove = () => {
+    document.execCommand('unlink');
+    this.setState({showModal: false, url: ''});
   }
 
   private handleItalicClick = () => {
@@ -38,14 +111,6 @@ class RichTextEditor extends React.PureComponent<Props, State> {
 
   private handleBoldClick = () => {
     document.execCommand('bold', false);
-  }
-
-  private handleInputChange = (event: React.FormEvent<HTMLElement>) => {
-    // tslint:disable-next-line
-    console.log(event.currentTarget.innerText, this.Editor);
-    this.setState({
-      html: event.currentTarget.innerText ? event.currentTarget.innerText : null,
-    });
   }
 }
 
--- src/components/RichTextEditor/components/Editor/Editor.tsx
+++ src/components/RichTextEditor/components/Editor/Editor.tsx
@@ -2,21 +2,17 @@ import * as React from 'react';
 import './Editor.css';
 
 export interface Props {
-  isBold: boolean;
-  onChange(event: React.FormEvent<HTMLElement>): void;
-  onKeyDown?(event: React.FormEvent<HTMLElement>): void;
-  onKeyUp?(event: React.FormEvent<HTMLElement>): void;
-  setEditor?(element: HTMLDivElement): void;
+  onMouseChange(event: React.FormEvent<HTMLElement>): void;
+  setEditor(element: HTMLDivElement): void;
 }
 
-export default function Editor({onChange, onKeyDown, onKeyUp, setEditor}: Props) {
+export default function Editor({onMouseChange, setEditor}: Props) {
   return (
     <div 
       className="Editor" 
       contentEditable={true} 
-      onInput={onChange}
-      onKeyDown={onKeyDown}
-      onKeyUp={onKeyUp}
+      onMouseDown={onMouseChange}
+      onMouseUp={onMouseChange}
       ref={setEditor}
     />
   );
